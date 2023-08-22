e3bcfacd46bf95947359c4ba9de7311d6333f7be
Thiago Pereira <iamthiago2@gmail.com>
2023-08-22 10:07:00 -0300

    Banco de dados

---
+++ src/bancodedados/instrutores.js
@@ -0,0 +1,19 @@
+const bancoDeDados = {
+    idAtual: 3,
+    instrutores: [
+        {
+            id: 1,
+            nome: "Thiago",
+            email: "thiago@email.com",
+            status: true,
+        },
+        {
+            id: 2,
+            nome: "Eduarda",
+            email: "eduarda@email.com",
+            status: true,
+        },
+    ],
+};
+
+module.exports = bancoDeDados;
